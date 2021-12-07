539f87e9d90ff19da3d558f81336784097832f66
Denis <dsapaiv@gmail.com>
2021-12-07 19:27:03 +0200

    hex pm publishing config

--- mix.exs
+++ mix.exs
@@ -7,23 +7,32 @@ defmodule Roboxir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Roboxir",
+      source_url: "https://github.com/Legendree/Roboxir",
+      description: description(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :inets, :ssl],
+      extra_applications: [:inets, :ssl],
       mod: {Roboxir.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
+  end
+
+  defp description() do
+    "0 dependencies, straightforward Robots.txt parser."
+  end
+
+  defp package() do
+    [licenses: ["Apache-2.0"], links: %{"GitHub" => "https://github.com/Legendree/Roboxir"}]
   end
 end
